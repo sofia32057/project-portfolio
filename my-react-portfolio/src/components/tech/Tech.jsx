adbfe885b1419a2ed901127b4759c6c6016ac23f
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-03-25 22:05:20 +0100

    Update - Tech responsive

--- my-react-portfolio/src/components/tech/Tech.jsx
+++ my-react-portfolio/src/components/tech/Tech.jsx
@@ -1,13 +1,16 @@
-import "./tech.css"
+import "./tech.css";
 
 export const Tech = () => {
   return (
     <section className="tech">
-      <h2>Tech</h2>
-      <p>HTML, CSS, Flexbox, JavaScript, ES6, 
-JSX, React, React Hooks, Node.js, 
-Mongo DB, Web Accessibility, APIs, 
-mob-programming, pair-programming, GitHub. </p>
+      <div className="tech-container">
+        <h2>Tech</h2>
+        <p>
+          HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js,
+          Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming,
+          GitHub.
+        </p>
+      </div>
     </section>
-  )
-}
+  );
+};
