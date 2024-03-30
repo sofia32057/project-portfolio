16488f046a10a4632f107625950c5f95c23966b9
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-03-30 22:41:48 +0100

    Update - added default project image

--- my-react-portfolio/src/components/projects/ProjectImage.jsx
+++ my-react-portfolio/src/components/projects/ProjectImage.jsx
@@ -2,9 +2,9 @@ import "./projectImage.css";
 import projectImages from "../../project-images.json";
 
 export const ProjectImage = ({ repo, url, alt }) => {
-  const repoImage = projectImages.find(img => img.repoName === repo);
+  const repoImage = projectImages.find(img => img.repoName === repo || img.repoName === "default");
   return (
-    repoImage && (
+    (
       <a href={url} className="image-wrapper">
         <img
           className="project-image"
